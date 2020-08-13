ac0fb49022a40f277f0a220f2dd2d843f2db9ac2
Ganesh7625 <47949728+Ganesh7625@users.noreply.github.com>
2020-08-13 08:51:49 -0400

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,13 @@
+import "package:flutter/material.dart";
+import 'UI/Home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return profileApp();
+  }
+}
